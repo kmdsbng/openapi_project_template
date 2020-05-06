e6efd7153e7979ad56bc63a7e5e631630b6e705f
Yoshihiro Kameda <kameda.sbng@gmail.com>
2020-05-06 19:51:47 +0900

    add files

---
+++ README.md
@@ -0,0 +1,9 @@
+# OpenAPI project template
+
+To setup OpenAPI project:
+```
+bash <(curl -s https://raw.githubusercontent.com/kmdsbng/openapi_project_template/master/setup_openapi_project.sh)
+```
+
+
+
--- setup_openapi_project.sh
+++ setup_openapi_project.sh
@@ -1,7 +1,21 @@
 
 mkdir bin
-# curl -o 
+curl -o bin/dredd_hooks.js https://raw.githubusercontent.com/kmdsbng/openapi_project_template/master/template/bin/dredd_hooks.js
 
+curl -o bin/generate_kotlin_code.sh https://raw.githubusercontent.com/kmdsbng/openapi_project_template/master/template/bin/bin/generate_kotlin_code.sh
 
+curl -o bin/generate_ts_code.sh https://raw.githubusercontent.com/kmdsbng/openapi_project_template/master/template/bin/generate_ts_code.sh
+
+curl -o bin/start_dredd_api_test.sh https://raw.githubusercontent.com/kmdsbng/openapi_project_template/master/template/bin/start_dredd_api_test.sh
+
+curl -o bin/start_prism_mock_server.sh https://raw.githubusercontent.com/kmdsbng/openapi_project_template/master/template/bin/start_prism_mock_server.sh
+
+curl -o README.md https://raw.githubusercontent.com/kmdsbng/openapi_project_template/master/template/README.md
+
+mkdir openapi
+curl -o openapi/openapi.yaml https://raw.githubusercontent.com/kmdsbng/openapi_project_template/master/template/openapi/openapi.yaml
+
+echo "Setup is finished."
+echo "Please read README.md"
 
 
